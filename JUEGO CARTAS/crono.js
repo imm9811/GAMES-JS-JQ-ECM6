f5a1be7a9d45ef0904f8024f9fcb4118c54bc0a4
Ismael Moreno <imm9811@gmail.com>
2019-03-01 17:19:16 +0100

    Add files via upload

--- JUEGO CARTAS/crono.js
+++ JUEGO CARTAS/crono.js
@@ -3,47 +3,46 @@ var tiempo = {
     minuto: 0,
     segundo: 0
 };
-
-
-var running_time=null;
+var running_time = null;
+var bandera = false;
 $(document).ready(function () {
-    let bandera=null;
-  
-   if($("#deck").click()){
-       bandera=true;
-   }
-      if(bandera==true){
-        running_time = setInterval(function () {
-               
-            // Segundos
-            tiempo.segundo++;
-            if (tiempo.segundo >= 60) {
-                tiempo.segundo = 0;
-                tiempo.minuto++;
-            }
-            
-            // Minutos
-            if (tiempo.minuto >= 60) {
-                tiempo.minuto = 0;
-                tiempo.hora++;
-            }
+    $("#deck").click(function () {
+        if (bandera == true) {
+            //si entra que no haga nada para que no pete el crono
+        }
+        else {
+            bandera = true;
+            running_time = setInterval(function () {
 
-            $("#hour").html(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora);
-            $("#minute").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto);
-            $("#second").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
-        }, 1000);
-       
-    }
+                // Segundos
+                tiempo.segundo++;
+                if (tiempo.segundo >= 60) {
+                    tiempo.segundo = 0;
+                    tiempo.minuto++;
+                }
 
+                // Minutos
+                if (tiempo.minuto >= 60) {
+                    tiempo.minuto = 0;
+                    tiempo.hora++;
+                }
+
+                $("#hour").html(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora);
+                $("#minute").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto);
+                $("#second").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
+            }, 1000);
+
+        }
+
+    });
 });
-
-function reiniciarTiempo(){
+function reiniciarTiempo() {
     clearInterval(running_time);
     tiempo.hora = 0;
     tiempo.minuto = 0;
     tiempo.segundo = 0;
-    $("#hour").text(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora+" : ");
-    $("#minute").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto+" : ");
+    $("#hour").text(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora + " : ");
+    $("#minute").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto + " : ");
     $("#second").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
-   
+    bandera = false;
 }
--- JUEGO CARTAS/principal2.js
+++ JUEGO CARTAS/principal2.js
@@ -1,10 +1,10 @@
 //variables globales
-let contenido=[];
-let gatos = [];
-let contadorFallos = 0;
-let contadorAciertos = 0;
-let contadorClick = 0;
-let bandera=false;
+var contenido=[];
+var gatos = [];
+var contadorFallos = 0;
+var contadorAciertos = 0;
+
+
 //cargamos el ajax
 
 $(document).ready(function () {
@@ -15,31 +15,34 @@ $(document).ready(function () {
         cache: false,
         beforeSend: function () { console.log("loading file..."); },
         complete: function () { console.log("file loded") },
+        error:()=>console.log("archivos no cargados"),
         success: function (data, statusText) {
             contenido = data;
             colocarFotos();
         }
     });
-
+    $("#aciertos").text(contadorAciertos);
+    $("#fallos").text(contadorFallos);
+    
 });//fin function ready
 
 function colocarFotos() {
 
-    let texto = "";
-    let arrayFotos = [];
-    let apoyo = [];
+    var texto = "";
+    var arrayFotos = [];
+    var apoyo = [];
 
     //meto los variables en otro array auxliar para no tratar con el original
-    for (let index = 0; index < contenido.length; index++) {
-        for (let index2 = 0; index2 < 2; index2++) {
+    for (var index = 0; index < contenido.length; index++) {
+        for (var index2 = 0; index2 < 2; index2++) {
             apoyo.push(contenido[index]);
         }
     }
     //desordeno los valores dentro del array
     arrayFotos = apoyo.sort(function () { return Math.random() - 0.5 });
 
-    for (let index = 0; index < 12; index++) {
-        texto += `<input type="checkbox" onclick="pasarDatos('${arrayFotos[index].nombre}')" class="${arrayFotos[index].nombre}"><u><img src="${arrayFotos[index].archivo}"></u><b></b>`;
+    for (var index = 0; index < 12; index++) {
+        texto += `<input type="checkbox" id=${index} onclick="pasarDatos('${arrayFotos[index].nombre}',${index})" class="${arrayFotos[index].nombre}"><u><img src="${arrayFotos[index].archivo}"></u><b></b>`;
     }
     //pongo todas las fotos en el html
     $('#deck').append(texto);
@@ -49,8 +52,11 @@ function colocarFotos() {
 } //fin colocarFotos
 
 //metodo que tiene todos los checkboxs en el cual se le pasa el valor de cada uno
-function pasarDatos(nombreGatos) {
+ 
 
+function pasarDatos(nombreGatos,id) {
+    $(`#${id}`).prop('disabled', true);
+    
     gatos.push(nombreGatos);
     //en el momento en el que meto dos gatos, deshablito todos los check para que no se metan mas nombre y compruebo
     if (gatos.length == 2) {
@@ -64,33 +70,39 @@ function ocultarGato(gato1, gato2) {
 //si son DISTINTOS acumula fallo, y vuelve a darse la vuelta
     if (gato1 != gato2) {
         contadorFallos++;
-        console.log("fallos"+contadorFallos);
+        $("#fallos").text(contadorFallos);
         $(`.${gato1}`).prop("checked", false);
 
         $(`.${gato2}`).prop("checked", false);
+      
     }
     //si son iguales SIGUEN deshabilitado y se bloquea para no poder seguir usandolo
 
     $("input[type=checkbox]").each(function (index) {
         if (this.checked == false) {
-            
+        
             this.disabled = false;
         }
        
     });
     if (gato1 == gato2) {
         contadorAciertos++;
+       
+        $("#aciertos").text(contadorAciertos);
+        
         if(contadorAciertos==6){
+        
+            clearInterval(running_time);
+            $("#resultado").text("¡Terminaste!").css("background-color", "yellow");
         }
     }
     gatos = [];
 }
 
 function reiniciar() {
-    let gatos = [];
-    let contadorFallos = 0;
-    let contadorAciertos = 0;
-    let contadorClick = 0;
+     gatos = [];
+     contadorFallos = 0;
+     contadorAciertos = 0;
     $("input[type=checkbox]").each(function () {
         this.checked=false;
         this.disabled = false;
@@ -98,6 +110,9 @@ function reiniciar() {
     reiniciarTiempo();
     $("#deck").empty();
     colocarFotos();
+    $("#aciertos").text(contadorAciertos);
+    $("#fallos").text(contadorFallos);
+    $("#resultado").text("").css("background-color", "none");
 }
 
 
